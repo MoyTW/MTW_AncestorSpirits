635eaaa1acfce7dc6dbe92ae018a99545c7c5175
MoyTW <moytravis@gmail.com>
2016-08-25 21:20:24 -0700

    Add placeholder building+art
    Has no functionality yet!

---
+++ Source/MTW_AncestorSpirits/Building_Shrine.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace MTW_AncestorSpirits
+{
+    public class Building_Shrine : Building
+    {
+    }
+}
