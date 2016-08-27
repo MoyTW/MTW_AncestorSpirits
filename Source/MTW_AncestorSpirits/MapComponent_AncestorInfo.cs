78edb4eddc0c72054c09f1bc991736e354a86983
MoyTW <moytravis@gmail.com>
2016-08-26 18:08:15 -0700

    Add a MapComponent + Injector

---
+++ Source/MTW_AncestorSpirits/MapComponent_AncestorInfo.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTW_AncestorSpirits
+{
+    class MapComponent_AncestorInfo : MapComponent
+    {
+        public override void MapComponentTick()
+        {
+            Log.Message("Ancestor info is loaded!");
+        }
+    }
+}
