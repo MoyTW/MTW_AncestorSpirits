40a1fa4128a640dba0fb82a456a7190ab01cae90
MoyTW <moytravis@gmail.com>
2016-09-08 20:19:23 -0700

    Delta events fire "soon" after delta triggered
    Also pretty much rewrote the whole generation loop.

--- Source/MTW_AncestorSpirits/EventTimer.cs
+++ Source/MTW_AncestorSpirits/EventTimer.cs
@@ -11,15 +11,58 @@ namespace MTW_AncestorSpirits
 {
     class EventTimer : IExposable
     {
-        private int ticksUntilNext = 0;
-        private bool canFireDelta = true;
-        private Random _random = null;
+        #region Data Types
 
         private enum EventType
         {
             positive = 0,
-            negative
+            negative,
+            undecided
         }
+
+        private enum EventCause
+        {
+            delta = 0,
+            timer
+        }
+
+        // Does ScribeRef allow you to just scribe it as a...uh, ref?
+        private class Event : IExposable
+        {
+            public int ttl;
+            public EventType type;
+            public EventCause cause;
+
+            public Event()
+            {
+                this.ttl = (int)AncestorConstants.EVENT_TIMER_TICKS_BETWEEN;
+                this.type = EventType.undecided;
+                this.cause = EventCause.timer;
+            }
+
+            public Event(int ttl, EventType type, EventCause cause)
+            {
+                this.ttl = ttl;
+                this.type = type;
+                this.cause = cause;
+            }
+
+            public void ExposeData()
+            {
+                Scribe_Values.LookValue<int>(ref this.ttl, "ttl", (int)AncestorConstants.EVENT_TIMER_TICKS_BETWEEN);
+                Scribe_Values.LookValue<EventType>(ref this.type, "type", EventType.undecided);
+                Scribe_Values.LookValue<EventCause>(ref this.cause, "cause", EventCause.timer);
+            }
+        }
+
+        #endregion
+
+        #region Vars & Accessors
+
+        private Event nextEvent = null;
+        private Event prevEvent = null;
+        private Random _random = null;
+
 
         private Random Random
         {
@@ -33,21 +76,50 @@ namespace MTW_AncestorSpirits
             }
         }
 
-        private int TicksToNextEvent()
+        private bool CanScheduleDelta
         {
-            var multiplier = (this.Random.NextDouble() - 0.5) * 2;
-            return (int)(multiplier * AncestorConstants.EVENT_TIMER_TICKS_PLUS_MINUS);
+            get
+            {
+                return (this.nextEvent.cause != EventCause.delta &&
+                    this.prevEvent != null &&
+                    this.prevEvent.cause != EventCause.delta);
+            }
         }
 
-        private void ScheduleNextEvent()
-        {
-            this.ticksUntilNext = this.TicksToNextEvent();
-        }
+        #endregion
 
         public EventTimer()
         {
-            this.ticksUntilNext = (int)AncestorConstants.EVENT_TIMER_DAYS_BEFORE_FIRST + this.TicksToNextEvent();
+            int timeToFirst = (int)AncestorConstants.EVENT_TIMER_DAYS_BEFORE_FIRST + this.GenTimerTicks();
+            this.nextEvent = new Event(timeToFirst, EventType.undecided, EventCause.timer);
         }
+
+        #region Event Generation
+
+        private int GenTimerTicks()
+        {
+            var multiplier = (this.Random.NextDouble() - 0.5) * 2;
+            return (int)(multiplier * AncestorConstants.EVENT_TIMER_TICKS_PLUS_MINUS) + (int)AncestorConstants.EVENT_TIMER_TICKS_BETWEEN;
+        }
+
+        private Event GenTimerEvent()
+        {
+            return new Event(this.GenTimerTicks(), EventType.undecided, EventCause.timer);
+        }
+
+        // Generates an event which will fire between 1 and 2 hours from generation time
+        private int GenDeltaTicks()
+        {
+            int ticksInHour = (int)(this.Random.NextDouble() * (double)AncestorConstants.TICKS_PER_HOUR);
+            return ticksInHour + AncestorConstants.TICKS_PER_HOUR;
+        }
+
+        private Event GenDeltaEvent(EventType type)
+        {
+            return new Event(this.GenDeltaTicks(), type, EventCause.delta);
+        }
+
+        #endregion
 
         private void TryForceIncident(String name)
         {
@@ -57,9 +129,23 @@ namespace MTW_AncestorSpirits
             incidentDef.Worker.TryExecute(incidentParams);
         }
 
-        private void FireEvent(EventType type)
+        private void FireNextEvent(AncestorApproval approval)
         {
-            if (type == EventType.positive)
+            // If undecided, resolve immediately
+            if (this.nextEvent.type == EventType.undecided)
+            {
+                var hourHistoryDelta = approval.HourHistoryDelta();
+                if (hourHistoryDelta >= 0.0)
+                {
+                    this.nextEvent.type = EventType.positive;
+                }
+                else
+                {
+                    this.nextEvent.type = EventType.negative;
+                }
+            }
+
+            if (this.nextEvent.type == EventType.positive)
             {
                 this.TryForceIncident("ResourcePodCrash");
             }
@@ -67,48 +153,36 @@ namespace MTW_AncestorSpirits
             {
                 this.TryForceIncident("Flashstorm");
             }
-            this.ScheduleNextEvent();
+
+            this.prevEvent = this.nextEvent;
+            this.nextEvent = this.GenTimerEvent();
         }
 
         public void UpdateTimer(AncestorApproval approval)
         {
             double intervalDelta = approval.IntervalDelta;
-            this.ticksUntilNext -= AncestorConstants.TICK_INTERVAL;
+            this.nextEvent.ttl -= AncestorConstants.TICK_INTERVAL;
 
-            // Shouldn't trigger immediately - should schedule for next hour
-            if (this.canFireDelta && approval.IntervalDelta > AncestorConstants.EVENT_TRIGGER_GAIN_INTERVAL_DELTA)
+            if (this.nextEvent.ttl <= 0)
             {
-                Log.Message("Immediate positive event!");
-                this.FireEvent(EventType.positive);
-                this.canFireDelta = false;
+                this.FireNextEvent(approval);
             }
-            else if (this.canFireDelta && approval.IntervalDelta < AncestorConstants.EVENT_TRIGGER_LOSS_INTERVAL_DELTA)
+            else if (approval.IntervalDelta > AncestorConstants.EVENT_TRIGGER_GAIN_INTERVAL_DELTA &&
+                this.CanScheduleDelta)
             {
-                Log.Message("Immediate negative event!");
-                this.FireEvent(EventType.negative);
-                this.canFireDelta = false;
+                this.nextEvent = this.GenDeltaEvent(EventType.positive);
             }
-            else if (this.ticksUntilNext <= 0)
+            else if (approval.IntervalDelta < AncestorConstants.EVENT_TRIGGER_LOSS_INTERVAL_DELTA &&
+                this.CanScheduleDelta)
             {
-                var hourHistoryDelta = approval.HourHistoryDelta();
-                Log.Message("Triggering event - delta = " + hourHistoryDelta);
-                if (hourHistoryDelta >= 0.0)
-                {
-                    this.FireEvent(EventType.positive);
-                }
-                else
-                {
-                    this.FireEvent(EventType.negative);
-                }
-                this.canFireDelta = true;
+                this.nextEvent = this.GenDeltaEvent(EventType.negative);
             }
         }
 
         public void ExposeData()
         {
-            Scribe_Values.LookValue<int>(ref this.ticksUntilNext, "ticksUntilNext",
-                (int)(AncestorConstants.EVENT_TIMER_DAYS_BEFORE_FIRST * AncestorConstants.EVENT_TIMER_TICKS_BETWEEN));
-            Scribe_Values.LookValue<bool>(ref this.canFireDelta, "canFireDelta", true);
+            Scribe_Deep.LookDeep<Event>(ref this.nextEvent, "nextEvent", new object[0]);
+            Scribe_Deep.LookDeep<Event>(ref this.prevEvent, "prevEvent", new object[0]);
         }
     }
 }
