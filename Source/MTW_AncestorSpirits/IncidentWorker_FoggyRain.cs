304eb4e2d405bbc786cfaa4fb8daf04bb4f182f6
MoyTW <moytravis@gmail.com>
2016-09-12 09:18:15 -0700

    Create IncidentWorker for Rain petition
    It works! Now to figure out how to do durations...

---
+++ Source/MTW_AncestorSpirits/IncidentWorker_FoggyRain.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTW_AncestorSpirits
+{
+    class IncidentWorker_FoggyRain : IncidentWorker
+    {
+        public override bool TryExecute(IncidentParms parms)
+        {
+            Find.WeatherManager.TransitionTo(DefDatabase<WeatherDef>.GetNamed("FoggyRain"));
+            return true;
+        }
+    }
+}
