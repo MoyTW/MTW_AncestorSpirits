e7eedd2eb5b7684f1544d7b65ee6a3d0d77f8ac4
MoyTW <moytravis@gmail.com>
2016-09-12 21:34:02 -0700

    Add CleanseSkies
    Balancing later, development now!

---
+++ Source/MTW_AncestorSpirits/IncidentWorker_CleanseSkies.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using Verse;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTW_AncestorSpirits
+{
+    class IncidentWorker_CleanseSkies : IncidentWorker
+    {
+        public override bool TryExecute(IncidentParms parms)
+        {
+            // How OP is this thing? Extremely OP. Will balance later.
+            List<MapCondition> conditions = Find.MapConditionManager.ActiveConditions.ToList<MapCondition>();
+            foreach (var condition in conditions)
+            {
+                if (!condition.Permanent)
+                {
+                    condition.End();
+                }
+            }
+
+            Find.WeatherManager.TransitionTo(WeatherDefOf.Clear);
+
+            return true;
+        }
+    }
+}
