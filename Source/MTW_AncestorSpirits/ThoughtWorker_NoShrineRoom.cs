cd4488ec5a656f00c535df7b638b19db9752afee
MoyTW <moytravis@gmail.com>
2016-09-19 21:13:50 -0700

    Add Very Bad thought if no shrine room

---
+++ Source/MTW_AncestorSpirits/ThoughtWorker_NoShrineRoom.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using Verse;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTW_AncestorSpirits
+{
+    class ThoughtWorker_NoShrineRoom : ThoughtWorker
+    {
+        private static RoomRoleDef shrineRoomDef = DefDatabase<RoomRoleDef>.GetNamed("MTW_ShrineRoom");
+
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            // TODO: There's gotta be a better way of doin' this!
+            if (p.def.defName != "Spirit") { return ThoughtState.Inactive; }
+
+            var shrine = Find.Map.GetComponent<MapComponent_AncestorTicker>().CurrentSpawner;
+            if (shrine == null) { return ThoughtState.Inactive; }
+
+            // HACK ALERT! Change Shrines to have an Interaction cell, and use that instead of a random one!
+            var room = RoomQuery.RoomAtFast(shrine.RandomAdjacentCellCardinal());
+            if (room == null)
+            {
+                return ThoughtState.ActiveAtStage(1);
+            }
+            else if (room.Role != shrineRoomDef)
+            {
+                return ThoughtState.ActiveAtStage(1);
+            }
+            else
+            {
+                return ThoughtState.Inactive;
+            }
+        }
+    }
+}
