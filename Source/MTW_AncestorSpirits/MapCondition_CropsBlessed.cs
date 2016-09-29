32f1ea2f1883f25c31f287382e71b874b66f4193
MoyTW <moytravis@gmail.com>
2016-09-28 22:11:56 -0700

    Add crop blessing
    Causes faster growth, slow healing (crops will be resistent to cold/heat,
    but will still *eventually* die)
    
    Unfortunately to change blights I'd have to override the blight worker
    class. Honestly I'd like to do less overriding rather than more.

---
+++ Source/MTW_AncestorSpirits/MapCondition_CropsBlessed.cs
@@ -0,0 +1,66 @@
+﻿using Verse;
+using RimWorld;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTW_AncestorSpirits
+{
+    class MapCondition_CropsBlessed : MapCondition
+    {
+        private const int HealInterval = 220;
+        private const float DesiredGrowthMultiplier = 2.5f;
+
+        private List<Thing> blessedPlants;
+
+        public override void Init()
+        {
+            var plants = Find.Map.listerThings.ThingsInGroup(ThingRequestGroup.CultivatedPlant);
+            this.blessedPlants = new List<Thing>(plants);
+        }
+
+        public override void MapConditionTick()
+        {
+            for (int i = this.blessedPlants.Count - 1; i >= 0; i--)
+            {
+                Plant p = (Plant)this.blessedPlants[i];
+
+                if (p.Destroyed)
+                {
+                    this.blessedPlants.RemoveAt(i);
+                }
+                else
+                {
+                    // Accelerate Growth
+                    if (p.LifeStage == PlantLifeStage.Growing)
+                    {
+                        float growthPerTick = (1f / (GenDate.TicksPerDay * p.def.plant.growDays)) * p.GrowthRate;
+                        float deltaGrowth = (DesiredGrowthMultiplier - 1f) * growthPerTick;
+                        p.Growth += deltaGrowth;
+                    }
+                    // Heal Minor Damage
+                    if (Find.TickManager.TicksGame % HealInterval == 0 && p.HitPoints < p.MaxHitPoints)
+                    {
+                        p.HitPoints += 1;
+                    }
+                }
+            }
+            if (this.blessedPlants.Count == 0)
+            {
+                // 1 tick less than "Permenant" check
+                this.duration = 999999999;
+            }
+        }
+
+        public override string TooltipString
+        {
+            get
+            {
+                return base.TooltipString + "\n\nNumber of currently blessed crops: " + this.blessedPlants.Count;
+            }
+        }
+
+    }
+}
