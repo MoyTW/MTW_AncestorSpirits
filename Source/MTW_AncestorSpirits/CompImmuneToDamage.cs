1136a58ba96c0ad73b37a6192d3240916d25a6eb
MoyTW <moytravis@gmail.com>
2016-11-05 12:21:59 -0700

    Make Ancestors immune to damage
    Add a ImmuneToDamage comp which just flat-out absorbs all attacks. I just
    realized that Comps exist and are pretty okay! Would have done it this way
    originally if I'd known.

---
+++ Source/MTW_AncestorSpirits/CompImmuneToDamage.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace MTW_AncestorSpirits
+{
+    class CompImmuneToDamage : ThingComp
+    {
+        public override void PostPreApplyDamage(DamageInfo dinfo, out bool absorbed)
+        {
+            base.PostPreApplyDamage(dinfo, out absorbed);
+            absorbed = true;
+        }
+    }
+}
---
+++ Source/MTW_AncestorSpirits/CompProperties_ImmuneToDamage.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace MTW_AncestorSpirits
+{
+    class CompProperties_ImmuneToDamage : CompProperties
+    {
+        public CompProperties_ImmuneToDamage()
+        {
+            this.compClass = typeof(CompImmuneToDamage);
+        }
+    }
+}
